fd049148165db49ab30bfbf512ca410672175a60
André Pinheiro <andrepdpinheiro@gmail.com>
2019-04-04 23:45:18 +0100

    Started to code the main page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400,600");
+
+:root {
+  --color-primary: #eb2f64;
+  --color-primary-light: #FF3366;
+  --color-primary-dark: #BA265D;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+html {
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+
+body {
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 400;
+  line-height: 1.6;
+  background-image: linear-gradient(to right bottom, var(--color-primary-light), var(--color-primary-dark));
+  background-size: cover;
+  background-repeat: no-repeat;
+  min-height: 100vh;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,111 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <header class="header">
+      <nav class="menu">
+        <ul class="menu-nav">
+          <li class="menu-nav__item">
+            <span>O Museu</span>
+          </li>
+
+          <li class="menu-nav__item">
+            <span>Agenda</span>
+          </li>
+
+          <img class="logo" src="../assets/logo_museu.png" alt="Logo Museu">
+
+          <li class="menu-nav__item">
+            <span>Galeria</span>
+          </li>
+
+          <li class="menu-nav__item">
+            <span>Notícias</span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <div class="content">
+      <main>
+      </main>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: { HelloWorld }
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss">
+  .container {
+    max-width: 160rem;
+    margin: 8rem auto;
+    background-color: #fff;
+
+    min-height: 100vh;
+  }
+
+  .header {
+    font-size: 1.5rem;
+    height: 9rem;
+    background-color: #191919;
+  }
+
+  .menu {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .logo {
+      width: 10rem;
+      height: 6rem;
+    }
+
+    .menu-nav {
+      position: relative;
+      width: 80%;
+      list-style: none;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+
+      &__item {
+        font-weight: 600;
+        text-transform: uppercase;
+        color: #969199;
+
+        span {
+          position: relative;
+          cursor: pointer;
+          transition: color .3s cubic-bezier(.445, .05, .05, .95);
+
+          &:hover {
+            color: #fff;
+
+            &::after {
+              width: 100%;
+            }
+          }
+
+          &::after {
+            content: "";
+            position: absolute;
+            height: .3rem;
+            width: 0;
+            background-color: #fff;
+            bottom: -1rem;
+            left: 0;
+
+            transition: width .3s cubic-bezier(.445, .05, .05, .95);
+          }
+        }
+      }
+    }
+  }
+</style>
