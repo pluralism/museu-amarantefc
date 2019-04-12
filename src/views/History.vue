5978db2c8ebee8d6e9df9dfb1834eee77ffa8ca4
André Pinheiro <andrepdpinheiro@gmail.com>
2019-04-13 00:32:04 +0100

    fixed bug with images in the timeline. added media query

--- src/views/History.vue
+++ src/views/History.vue
@@ -14,11 +14,11 @@
                     
                     <div class="history__timeline__list__item__container">
                         <div class="text">
-                        <p>Pompeu da Cunha Brochado foi o primeiro Presidente do clube, com apenas 19 anos. O campo de jogos do Amarante Futebol Clube, nos anos de 1923 e 1924 foi o campo da Praça. No dia 31 de Maio de 1923, o AFC fez o seu primeiro jogo contra o Santa Natália F.C. em que o Amarante venceu por 4-0.</p>
+                          <p>Pompeu da Cunha Brochado foi o primeiro Presidente do clube, com apenas 19 anos. O campo de jogos do Amarante Futebol Clube, nos anos de 1923 e 1924 foi o campo da Praça. No dia 31 de Maio de 1923, o AFC fez o seu primeiro jogo contra o Santa Natália F.C. em que o Amarante venceu por 4-0.</p>
                         </div>
 
                         <div class="image">
-                        <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/logotipo.jpg" alt="Timeline Image">
+                          <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/logotipo.jpg" alt="Timeline Image">
                         </div>
                     </div>
                 </li>
@@ -28,11 +28,11 @@
                     
                     <div class="history__timeline__list__item__container">
                         <div class="text">
-                        <p>Adelino Bacelar foi eleito Presidente do Clube. Dr. António Vahia foi Presidente do clube para a época 1925/26 e foi sob a sua presidência que o Amarante F.C., em 1925, conseguiu obter dos Serviços Florestais, por intermédio do Dr. Largo Cerqueira, e com o melhor acolhimento dos Engenheiros Florestais Luiz Virgolino, Rocha e Reimão, a cedência de uma faixa de terreno no Parque Florestal, com a superfície de 100 por 60 metros, para o seu campo desportivo.</p>
+                          <p>Adelino Bacelar foi eleito Presidente do Clube. Dr. António Vahia foi Presidente do clube para a época 1925/26 e foi sob a sua presidência que o Amarante F.C., em 1925, conseguiu obter dos Serviços Florestais, por intermédio do Dr. Largo Cerqueira, e com o melhor acolhimento dos Engenheiros Florestais Luiz Virgolino, Rocha e Reimão, a cedência de uma faixa de terreno no Parque Florestal, com a superfície de 100 por 60 metros, para o seu campo desportivo.</p>
                         </div>
 
                         <div class="image">
-                        <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/0025-campodafeira.jpg" alt="Timeline Image">
+                          <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/0025-campodafeira.jpg" alt="Timeline Image">
                         </div>
                     </div>
                 </li>
@@ -42,11 +42,11 @@
                     
                     <div class="history__timeline__list__item__container">
                         <div class="text">
-                        <p>O AFC joga no campo da Florestal – campo de jogos inaugurado em 12 de Julho de 1925 – um encontro de futebol entre o Amarante F.C. e o Voluntário F.C. de Vila Real. O resultado foi de 4-3, favorável ao Amarante.</p>
+                          <p>O AFC joga no campo da Florestal – campo de jogos inaugurado em 12 de Julho de 1925 – um encontro de futebol entre o Amarante F.C. e o Voluntário F.C. de Vila Real. O resultado foi de 4-3, favorável ao Amarante.</p>
                         </div>
 
                         <div class="image">
-                        <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/0035-equipa32_33.jpg" alt="Timeline Image">
+                          <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/0035-equipa32_33.jpg" alt="Timeline Image">
                         </div>
                     </div>
                 </li>
@@ -56,11 +56,11 @@
                     
                     <div class="history__timeline__list__item__container">
                         <div class="text">
-                        <p>Tenente Manuel Pereira é eleito presidente do clube, para as épocas de 1931/33. As instalações desportivas do Amarante F. C., desde Julho de 1931 até Janeiro de 1934, são no campo da Feira. Depois de um interregno, o Amarante Futebol Clube reaparece em Julho de 1931 contra uma Selecção de Penafiel. O Amarante venceu o desafio por 12 – 0.</p>
+                          <p>Tenente Manuel Pereira é eleito presidente do clube, para as épocas de 1931/33. As instalações desportivas do Amarante F. C., desde Julho de 1931 até Janeiro de 1934, são no campo da Feira. Depois de um interregno, o Amarante Futebol Clube reaparece em Julho de 1931 contra uma Selecção de Penafiel. O Amarante venceu o desafio por 12 – 0.</p>
                         </div>
 
                         <div class="image">
-                        <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/0038-equipa34_35.jpg" alt="Timeline Image">
+                          <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/0038-equipa34_35.jpg" alt="Timeline Image">
                         </div>
                     </div>
                 </li>
@@ -70,12 +70,12 @@
                     
                     <div class="history__timeline__list__item__container">
                         <div class="text">
-                        <p>Reaparece o clube em 23 de Janeiro de 1944, depois de um interregno de dois anos. D. Manuel Lobo da Silveira (Alvito), é eleito presidente do clube para o ano de 1944. Amadeu Faria é eleito presidente do clube, para os anos de 1945 e 1946. Inserido nas Festas do Junho de 1945, o Amarante Futebol Clube recebe a Associação Académica de Coimbra.</p>
-                        <p>Eng. Manuel Van Zeller Macedo é eleito presidente do clube para os anos de 1947 e 1948. Teófilo da Mota Freitas é eleito presidente do clube para o ano de 1949. Dr. Luís Van Zeller Macedo é eleito presidente do clube para o ano de 1950. Dr. José Soares Queiroz é eleito presidente do clube para o ano de 1951 e para a época 1952/53.</p>
+                          <p>Reaparece o clube em 23 de Janeiro de 1944, depois de um interregno de dois anos. D. Manuel Lobo da Silveira (Alvito), é eleito presidente do clube para o ano de 1944. Amadeu Faria é eleito presidente do clube, para os anos de 1945 e 1946. Inserido nas Festas do Junho de 1945, o Amarante Futebol Clube recebe a Associação Académica de Coimbra.</p>
+                          <p>Eng. Manuel Van Zeller Macedo é eleito presidente do clube para os anos de 1947 e 1948. Teófilo da Mota Freitas é eleito presidente do clube para o ano de 1949. Dr. Luís Van Zeller Macedo é eleito presidente do clube para o ano de 1950. Dr. José Soares Queiroz é eleito presidente do clube para o ano de 1951 e para a época 1952/53.</p>
                         </div>
 
                         <div class="image">
-                        <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/0032-equipa1952_53.jpg" alt="Timeline Image">
+                          <img src="http://www.pauloteixeira.net/amarantefc/wp-content/uploads/2010/07/0032-equipa1952_53.jpg" alt="Timeline Image">
                         </div>
                     </div>
                 </li>
@@ -108,6 +108,10 @@ export default class History extends Vue {}
       display: flex;
       flex-direction: column;
       align-items: center;
+
+      @media only screen and (max-width: 1000px) {
+        padding: 8rem 2rem;
+      }
 
       &__header {
         text-align: center;
@@ -185,11 +189,17 @@ export default class History extends Vue {}
               .image {
                 position: relative;
                 width: 50%;
-                padding: 0 2rem;
+                padding: 2.4rem 2rem;
 
                 display: flex;
                 justify-content: center;
                 align-items: center;
+
+                > img {
+                  max-width: 100%;
+                  max-width: 100%;
+                  object-fit: contain;
+                }
               }
             }
           }
