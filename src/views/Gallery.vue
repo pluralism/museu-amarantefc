1f7d50c164247d475a5f244150ea2a3703d8896a
André Pinheiro <andrepdpinheiro@gmail.com>
2019-04-14 14:14:35 +0100

    Starting to code the gallery page

---
+++ src/views/Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="gallery">
+        <p>teste</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({ name: 'gallery' })
+export default class Gallery extends Vue { }
+</script>
+
+<style lang="scss">
+    .gallery {
+        padding: 8rem;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,17 +16,18 @@ import { Component, Vue } from 'vue-property-decorator';
 import History from '@/views/History.vue';
 import Schedule from '@/views/Schedule.vue';
 import EventDetails from '@/views/EventDetails.vue';
+import Gallery from '@/views/Gallery.vue';
 
 import Footer from '@/components/Footer.vue';
 import Header from '@/components/Header.vue';
 
 @Component({
-  components: { History, Schedule, EventDetails, Header, Footer },
+  components: { History, Schedule, EventDetails, Gallery, Header, Footer },
 })
 export default class Home extends Vue {
   data() {
     return {
-      currentPage: 'eventDetails'
+      currentPage: 'gallery'
     };
   }
 }
