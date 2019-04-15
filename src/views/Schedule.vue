41ccea9e7c066d7f62cb60027e7e0222256f3b0f
André Pinheiro <andrepdpinheiro@gmail.com>
2019-04-15 23:11:48 +0100

    added flipping animation on the calendar

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -2,69 +2,75 @@
     <div class="schedule">
         <div class="calendar__wrapper">
             <div class="calendar">
-                <div class="calendar__controls">
-                    <button class="calendar__controls__prev">
-                        <svg width="14" height="10" viewBox="0 0 14 10">
-                            <path d="M5.06 0l.891.881-3.529 3.494L14 4.432v1.25L2.422 5.625l3.529 3.494-.892.881L0 5z" fill="#2c272f" fill-rule="evenodd"></path>
-                        </svg>
-                    </button>
+                <div class="calendar__front" :class="{'show-details': flipped}">
+                    <div class="calendar__controls">
+                        <button class="calendar__controls__prev">
+                            <svg width="14" height="10" viewBox="0 0 14 10">
+                                <path d="M5.06 0l.891.881-3.529 3.494L14 4.432v1.25L2.422 5.625l3.529 3.494-.892.881L0 5z" fill="#2c272f" fill-rule="evenodd"></path>
+                            </svg>
+                        </button>
 
-                    <div class="calendar__controls__date">
-                        <span>Abril 2019</span>
+                        <div class="calendar__controls__date">
+                            <span>Abril 2019</span>
+                        </div>
+
+                        <button class="calendar__controls__next">
+                            <svg width="14" height="10" viewBox="8 10 14 10">
+                                <path d="M16.94 10l-.891.881 3.529 3.494L8 14.432v1.25l11.578-.057-3.529 3.494.892.881L22 15z" fill="#2c272f" fill-rule="evenodd"></path>
+                            </svg>
+                        </button>
                     </div>
 
-                    <button class="calendar__controls__next">
-                        <svg width="14" height="10" viewBox="8 10 14 10">
-                            <path d="M16.94 10l-.891.881 3.529 3.494L8 14.432v1.25l11.578-.057-3.529 3.494.892.881L22 15z" fill="#2c272f" fill-rule="evenodd"></path>
-                        </svg>
-                    </button>
+                    <div class="calendar__days">
+                        <div class="calendar__days__item calendar__days__item--header">Dom</div>
+                        <div class="calendar__days__item calendar__days__item--header">Seg</div>
+                        <div class="calendar__days__item calendar__days__item--header">Ter</div>
+                        <div class="calendar__days__item calendar__days__item--header">Qua</div>
+                        <div class="calendar__days__item calendar__days__item--header">Qui</div>
+                        <div class="calendar__days__item calendar__days__item--header">Sex</div>
+                        <div class="calendar__days__item calendar__days__item--header">Sáb</div>
+
+
+                        <div class="calendar__days__item calendar__days__item--prev-month">31</div>
+                        <div class="calendar__days__item">1</div>
+                        <div class="calendar__days__item">2</div>
+                        <div class="calendar__days__item">3</div>
+                        <div class="calendar__days__item">4</div>
+                        <div class="calendar__days__item">5</div>
+                        <div class="calendar__days__item">6</div>
+                        <div class="calendar__days__item">7</div>
+                        <div class="calendar__days__item">8</div>
+                        <div class="calendar__days__item">9</div>
+                        <div class="calendar__days__item">10</div>
+                        <div class="calendar__days__item">11</div>
+                        <div class="calendar__days__item">12</div>
+                        <div class="calendar__days__item">13</div>
+                        <div class="calendar__days__item">14</div>
+                        <div class="calendar__days__item calendar__days__item--today" @click="flipped = !flipped">15</div>
+                        <div class="calendar__days__item">16</div>
+                        <div class="calendar__days__item">17</div>
+                        <div class="calendar__days__item">18</div>
+                        <div class="calendar__days__item">19</div>
+                        <div class="calendar__days__item">20</div>
+                        <div class="calendar__days__item">21</div>
+                        <div class="calendar__days__item">22</div>
+                        <div class="calendar__days__item">23</div>
+                        <div class="calendar__days__item">24</div>
+                        <div class="calendar__days__item">25</div>
+                        <div class="calendar__days__item">26</div>
+                        <div class="calendar__days__item">27</div>
+                        <div class="calendar__days__item">28</div>
+                        <div class="calendar__days__item">29</div>
+                        <div class="calendar__days__item">30</div>
+                        <div class="calendar__days__item calendar__days__item--next-month">1</div>
+                        <div class="calendar__days__item calendar__days__item--next-month">2</div>
+                        <div class="calendar__days__item calendar__days__item--next-month">3</div>
+                        <div class="calendar__days__item calendar__days__item--next-month">4</div>
+                    </div>
                 </div>
 
-                <div class="calendar__days">
-                    <div class="calendar__days__item calendar__days__item--header">Dom</div>
-                    <div class="calendar__days__item calendar__days__item--header">Seg</div>
-                    <div class="calendar__days__item calendar__days__item--header">Ter</div>
-                    <div class="calendar__days__item calendar__days__item--header">Qua</div>
-                    <div class="calendar__days__item calendar__days__item--header">Qui</div>
-                    <div class="calendar__days__item calendar__days__item--header">Sex</div>
-                    <div class="calendar__days__item calendar__days__item--header">Sáb</div>
-
-
-                    <div class="calendar__days__item calendar__days__item--prev-month">31</div>
-                    <div class="calendar__days__item">1</div>
-                    <div class="calendar__days__item">2</div>
-                    <div class="calendar__days__item">3</div>
-                    <div class="calendar__days__item">4</div>
-                    <div class="calendar__days__item">5</div>
-                    <div class="calendar__days__item">6</div>
-                    <div class="calendar__days__item">7</div>
-                    <div class="calendar__days__item">8</div>
-                    <div class="calendar__days__item">9</div>
-                    <div class="calendar__days__item">10</div>
-                    <div class="calendar__days__item">11</div>
-                    <div class="calendar__days__item">12</div>
-                    <div class="calendar__days__item">13</div>
-                    <div class="calendar__days__item">14</div>
-                    <div class="calendar__days__item calendar__days__item--today">15</div>
-                    <div class="calendar__days__item">16</div>
-                    <div class="calendar__days__item">17</div>
-                    <div class="calendar__days__item">18</div>
-                    <div class="calendar__days__item">19</div>
-                    <div class="calendar__days__item">20</div>
-                    <div class="calendar__days__item">21</div>
-                    <div class="calendar__days__item">22</div>
-                    <div class="calendar__days__item">23</div>
-                    <div class="calendar__days__item">24</div>
-                    <div class="calendar__days__item">25</div>
-                    <div class="calendar__days__item">26</div>
-                    <div class="calendar__days__item">27</div>
-                    <div class="calendar__days__item">28</div>
-                    <div class="calendar__days__item">29</div>
-                    <div class="calendar__days__item">30</div>
-                    <div class="calendar__days__item calendar__days__item--next-month">1</div>
-                    <div class="calendar__days__item calendar__days__item--next-month">2</div>
-                    <div class="calendar__days__item calendar__days__item--next-month">3</div>
-                    <div class="calendar__days__item calendar__days__item--next-month">4</div>
+                <div class="calendar__back" :class="{ 'show-details': flipped }">
+                    <p @click="flipped = !flipped">Voltar</p>
                 </div>
             </div>
         </div>
@@ -156,12 +162,47 @@
 import { Vue, Component } from 'vue-property-decorator';
 
 @Component({ name: 'schedule' })
-export default class Schedule extends Vue { }
+export default class Schedule extends Vue {
+    data() {
+        return {
+            flipped: false
+        };
+    }
+}
 </script>
 
 <style lang="scss">
     .calendar {
+        position: relative;
+
+        &__front {
+            backface-visibility: hidden;
+            transform: rotateX(0deg);
+            transition: transform .3s ease-in-out;
+
+            &.show-details {
+                transform: rotateX(180deg);
+            }
+        }
+
+        &__back {
+            backface-visibility: hidden;
+            transform: rotateX(180deg);
+            transition: transform .3s ease-in-out;
+
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+
+            &.show-details {
+                transform: rotateX(0deg);
+            }
+        }
+
         &__wrapper {
+            position: relative;
             flex-basis: 20%;
             margin-right: 3rem;
             font-size: 1.2rem;
